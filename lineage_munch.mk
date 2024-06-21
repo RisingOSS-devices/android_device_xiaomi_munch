67cfcd02f7856f25cb65eaa2a35e183b80a832d7
erwinabs <erwinabubakarsidik@gmail.com>
2024-06-21 08:35:11 +0000

    munch: Update OS1.0.2.0.ULMMIXM

--- lineage_munch.mk
+++ lineage_munch.mk
@@ -22,4 +22,4 @@ PRODUCT_MODEL := POCO F4
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Redmi/munch/munch:13/RKQ1.211001.001/V14.0.6.0.TLMMIXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/munch/munch:13/RKQ1.231207.002/V816.0.1.0.ULMMIXM:user/release-keys
